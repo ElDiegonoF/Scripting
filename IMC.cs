92664b8acb178d71705ff4acfc3b82949f7a95c1
ElDiegonoF <68286902+ElDiegonoF@users.noreply.github.com>
2020-07-14 12:36:01 -0500

    Add files via upload

---
+++ IMC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace tutoria_3_semana_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos 
+            Console.WriteLine("Ingrese estadura en cm:");
+            Double estatura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese peso en Kg:");
+            double peso = double.Parse(Console.ReadLine());
+
+            //calculo IMC
+            Double Indice = (peso / (estatura * estatura));
+
+            if (Indice < 18.5)
+            {
+                Console.WriteLine("tas de bajo del peso normal ");
+            }
+            else if (18.5 < Indice && Indice < 24.9)
+            {
+                Console.WriteLine("Peso normal ");
+            }
+            else if (25.0 < Indice && Indice < 29.9)
+            {
+                Console.WriteLine("Tu peso es superior al normal ");
+            }
+            else 
+            {
+                Console.WriteLine("estás gordo ceboso");
+            }
+        }
+    }
+}
