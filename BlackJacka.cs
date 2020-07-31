47967582b6885b9d8769d2933c3bf88bffc935e3
ElDiegonoF <68286902+ElDiegonoF@users.noreply.github.com>
2020-07-31 07:57:15 -0500

    Add files via upload

---
+++ BlackJacka.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace BlackJack1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            bool gameover = false;
+            int carta1 = aleatorio.Next(1, 11);
+            int carta2 = aleatorio.Next(1, 11);
+            int baraja = 0, total = (carta1 + carta2);;
+            Console.Write(" carta1: " + carta1);
+            Console.Write(" carta2: " + carta2);
+            Console.Write(" Total: " + total);
+
+            while (21 > total && gameover != true )
+            {   Console.WriteLine(" ¿Deseas una carta adicional? ");
+                string si = Console.ReadLine();
+
+                if (si == "s")
+                {
+                    baraja = aleatorio.Next(1, 11);
+                    total += baraja;
+                    Console.Write(" Carta= " + baraja);
+                    Console.Write(" Total= " + total);
+                }
+                else if (si == "n")
+                {
+                    Console.Write(" (Perdiste) ");
+                    gameover = true;
+                }
+  
+            }
+            if (21 == total)
+            {
+                Console.WriteLine(" (ganaste) ");
+            }
+            else if (total > 21)
+            {
+                Console.WriteLine(" (perdiste) ");
+            }
+
+            Console.WriteLine(" total: " + total);
+            Console.WriteLine(" Gracias por jugar ");
+        }
+    }
+}
