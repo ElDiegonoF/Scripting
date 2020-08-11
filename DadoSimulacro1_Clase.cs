843a537dffdf3de0ec7183c7d2cb9bbb5e6df33a
ElDiegonoF <68286902+ElDiegonoF@users.noreply.github.com>
2020-08-11 13:30:40 -0500

    Add files via upload

---
+++ DadoSimulacro1_Clase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Dado_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            bool gameover = false;
+            int t = 0;
+            int dado1 = 0;
+            int dado2 = 0;
+            int total = 0;
+
+            int a = 0;
+
+            while (100 > total && gameover != true)
+            {
+                Console.WriteLine(" ¿Quieres continuar? ");
+                string si = Console.ReadLine();
+
+                if (si == "s")
+                {
+                    dado1 = aleatorio.Next(1, 7);
+                    dado2 = aleatorio.Next(1, 7);
+                    total += (dado1 + dado2);
+                    t++;
+                    Console.WriteLine(" dado1= " + dado1 + "dado2= " + dado2);
+                    Console.WriteLine(" Total= " + total);
+
+                    if ((dado1 + dado2) > 6)
+                    {
+                        a++;
+                    }
+                    else if (dado1 == 1 && dado2 == 1)
+                    {
+                        Console.WriteLine(" (Perdiste) ");
+                        gameover = true;
+                    }
+                }
+                else if (si == "n")
+                {
+                    Console.WriteLine(" (Perdiste) ");
+                    gameover = true;
+                }
+            }
+            if (total > 99)
+            {
+                Console.WriteLine(" (ganaste) ");
+            }
+            double porcentaje = ((double) a / t)*100;
+            Console.WriteLine("porcentaje de turnos que sacaste más de 6" + porcentaje);
+            Console.WriteLine(" total: " + total);
+            Console.WriteLine(" Gracias por jugar ");
+        }
+    }
+}
---
+++ DadosSimulacro1_Casa.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            bool gameover = false;
+            int turno = 0;
+            int dado = 0;
+            int total = 0;
+
+            int a = 2;
+
+            while (100 > total || gameover != true)
+            {
+                if (total > 99 || a == 0)
+                {
+                    Console.WriteLine(" ganaste ");
+                    break;
+                }
+
+                Console.WriteLine(" ¿Quieres continuar? ");
+                string si = Console.ReadLine();
+
+                if (si == "s")
+                {
+                    dado = aleatorio.Next(11, 11);
+                    total += (dado);
+                    turno++;
+                    Console.WriteLine(" dado= " + dado);
+                    Console.WriteLine(" Total= " + total);
+                }
+                else if (si == "n")
+                {
+                Console.WriteLine(" (Perdiste) ");
+                    break;
+                }
+
+
+                if(dado == 12 && a == 2)
+                {
+                    a--;
+                    Console.Write(" saca ahora 10 y ganas.   ");
+                }
+                else if (dado == 10 && a == 1 )
+                {
+                  a--;   
+                }
+                else
+                {
+                    a = 2 ;
+                }
+
+                if (turno > 3)
+                {
+                    if ((dado % 2) == 0)
+                    {
+                        gameover = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine(" perdiste . ");
+                        
+                        break;
+                    }
+                }
+
+
+            }
+            Console.WriteLine("Turnos jugados" + turno);
+            Console.WriteLine(" total: " + total);
+            Console.WriteLine(" Gracias por jugar ");
+            
+        }
+    }
+}
+
