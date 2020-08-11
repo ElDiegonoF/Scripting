c3e0274ba0e8efea76a74405ae29118887e537ca
ElDiegonoF <68286902+ElDiegonoF@users.noreply.github.com>
2020-08-11 13:25:28 -0500

    Add files via upload

---
+++ DadosSimulacro2_Casa.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random aleatorio = new Random();
+
+            // dados 
+            int dado = 0;
+            int dado2 = 0;
+
+            int total = 0;//total acumulado
+
+            int l = 3; // vidas
+
+            int t = 2; //Turnos para perder vida
+
+            int p = 0; // Turnos para dos dados
+
+            int turnos = 0; //Turnos Jugados
+            
+
+            while (99 > total)
+            {
+
+                //dados
+            
+                Console.WriteLine(" ¿Desea seguir? ");
+                string yes = Console.ReadLine();
+                if (yes == "s")
+                {
+                    dado = aleatorio.Next(1, 7);
+                    Console.WriteLine(" (Dado): " + dado);
+                    total += (dado);
+                    t--;
+                    p++;
+                    turnos++;
+                }
+                else if (yes == "n")
+                {
+                    Console.WriteLine(" (Perdiste) ");
+                    break;
+                }
+                
+
+                //ciclo en el turno 3
+                if (p == 3)
+                {
+                    dado2 = aleatorio.Next(1, 7);
+                    Console.WriteLine(" (Dado2): " + dado2);
+                    total += (dado2);
+                    p = 0;
+                }
+                
+                Console.WriteLine(" total= " + total);
+                Console.WriteLine(" vidas= " + l);
+                Console.WriteLine(" Turnos Jugados : " + turnos);
+
+                if (t == 0)//Cuando el Jugador pierda una vida por cada 2 turnos
+                {
+                    l--;
+                    t = 2;
+                }
+
+                if (total > 99) // si el jugador supera los 100 puntos
+                {
+                    Console.WriteLine(" (Ganaste) ");
+                    break;
+                }
+
+                if (l == 0) //Si el jugador se queda sin vidas
+                {
+                    Console.WriteLine(" (Perdiste) ");
+                    break;
+                }
+
+                if((dado == dado2) && 3 > l)
+                {
+                    l++;
+                }
+
+            }
+
+            Console.WriteLine(" total: " + total);
+            Console.WriteLine(" (Gracias por jugar) ");
+            Console.WriteLine(" Quedaste con " + l + " vidas. ");
+            Console.WriteLine(" Turnos Jugados : " + turnos);
+        }
+    }
+}
---
+++ DadosSimulacro2_Clase.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();        
+
+            // dado 
+            int dado = 0;
+            
+
+            int i = 2; //comodín de usar los dados especiales
+
+            int total = 0;
+
+            int l = 3; // vidas
+
+            int uno = 0; //contador de si saca 1
+            int seis = 0; //contador de si saca doble 6
+
+            string si = "s";
+
+            while (99 > total)
+            {
+                
+                
+                //dados especiales
+                if (i !=0 )
+                {
+                    Console.WriteLine(" ¿Desea usar el comodín de dados especiales? ");
+                    string yes = Console.ReadLine();
+                    if (yes == "s")
+                    {
+                        dado = aleatorio.Next(1, 13);
+
+                        total += (dado);
+                        i--;
+
+                        
+
+                    }
+                    else if (yes == "n")
+                    {     
+                        dado = aleatorio.Next(1, 7);
+                        total += (dado);
+
+                        
+                    }
+                }
+
+                //ciclo dados normales
+                else if (i == 0)
+                {
+                    dado = aleatorio.Next(1, 7);
+                    total += (dado);
+
+                          
+                }
+
+                Console.WriteLine(" especial =" + dado);
+                Console.WriteLine(" total= " + total);
+                Console.WriteLine(" vidas= " + l);
+
+                //si saca doble 1 pierde una vida y pierde 10 puntos de total.
+                if (dado == 1 && uno == 0)
+                {
+                    uno++;
+                }
+                else if (dado == 1 && uno == 1)
+                {
+                    l --;
+                    total -= 10;
+                    uno = 0;
+                    Console.WriteLine(" Perdiste una vida y 10 puntos. Vidas:  "+ l);
+                }
+               
+                if(l == 0)
+                {
+                    Console.WriteLine(" Perdiste ");
+                    break;
+                }
+
+                //si saca doble 6 suma una vida (solo si no es menor a 3).
+                if(dado == 6 && seis == 0)
+                {
+                    seis++;             
+                }
+                else if ((dado == 6 && seis == 1) && 3 > l)
+                {
+                    l++;
+                    Console.WriteLine(" Ganaste 1 vida. Vidas:  " + l);
+                }
+                else
+                {
+                    seis = 0;
+                }
+
+                if (total > 99)
+                {
+                    Console.WriteLine(" ganaste ");
+                    break;
+                }
+
+                Console.WriteLine(" ¿Quieres continuar? ");
+                si = Console.ReadLine();
+                if (si == "n")
+                {
+                    Console.WriteLine(" (Perdiste) ");
+                    break;
+                }
+                else if(si =="s")
+                {
+                    continue;
+                }
+            }
+            
+            Console.WriteLine(" total: " + total);
+            Console.WriteLine(" Gracias por jugar ");
+        }
+    }
+}
