1f9f7bde5f7fb9a0a5443964dc9050b9bfc134fa
ElDiegonoF <68286902+ElDiegonoF@users.noreply.github.com>
2020-08-12 17:42:59 -0500

    Add files via upload

---
+++ BlackJack2.0.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Xml;
+
+namespace BlackJack2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+
+            int contador = 0;
+            string si = "s";
+
+            Console.WriteLine(" ¿Cuantos jugadores van a participar? (2 a 5)");//numero de jugadores a participar
+            int jugadores = int.Parse(Console.ReadLine());
+            while((jugadores < 2)|| (5 < jugadores))
+            {
+                Console.WriteLine("ERROR. mínimo 2 y máximo 5 jugadores: ");
+                jugadores = int.Parse(Console.ReadLine());
+            }
+
+            // Comienza el Juego
+            Console.WriteLine(" Comienza el Juego ");
+
+            while (contador < jugadores)
+            {
+                Console.WriteLine("\n\nBienvenido jugador " + (contador + 1));
+                int baraja = 0, total = 0; 
+
+                while (true)
+                {
+                    baraja = aleatorio.Next(1, 11);
+                    total += baraja;
+                    Console.Write(" Carta= " + baraja);
+                    Console.WriteLine(" total: " + total);
+
+                    if (baraja == 1) //si saca 1, sale
+                    {
+                        Console.WriteLine(" (Eliminado) ");
+                        Console.WriteLine(" total: " + total);
+                        Console.WriteLine(" Gracias por jugar ");
+                        break;
+                    }
+                    if ( 21 < total) //si llega a 21 concluye
+                    {
+                        Console.WriteLine(" (ganaste) ");
+                        Console.WriteLine(" total: " + total);
+                        Console.WriteLine(" Gracias por jugar ");
+                        break;
+                    }
+
+                    Console.WriteLine(" ¿Deseas continuar? (s/n) "); //continuar?
+                    si = Console.ReadLine();
+                    
+                    if (si == "n")
+                    {
+                        Console.WriteLine(" Retirado ");
+                        break;
+                    }
+                    else if (si == "s")
+                    {
+                        continue;
+                    }
+                }
+                contador++;
+
+            }
+        }
+    }
+}
---
+++ Captcha.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Captcha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Bienvenido al Captcha! | Por favor responda la siguiente operación: "); //bienvenida
+
+            Random aleatorio = new Random();
+
+            int i = 3; // contador de oportunidades
+            
+            while(true)
+            {
+                //datos a sumar para el captcha
+                int a = aleatorio.Next(0, 10);
+                int b = aleatorio.Next(0, 10);
+
+                int s = a + b; //suma correcta
+
+                Console.WriteLine("cuánto es:"+ a + " + " +b + "?");//operación
+                
+                int r = int.Parse(Console.ReadLine());
+
+                
+                if(r == s)
+                {
+                    Console.WriteLine(" Bienvenido al programa. ");
+                    break;
+                }
+                
+                if(r !=s)
+                {                     
+                    i--;
+                    Console.WriteLine(" ERROR | vuelve a intentar" + "te quedan " + i + "oportunidades"); 
+                    continue;
+                }
+
+                if(i == 0)
+                {
+                    Console.WriteLine(" ACCESO DENEGADO | ¡¡PIERDETE BOT!! ");
+                    break;
+                }
+            }
+        }
+    }
+}
