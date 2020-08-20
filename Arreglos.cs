4523066a0d57d19f813e4de4ab5c821f20903309
ElDiegonoF <68286902+ElDiegonoF@users.noreply.github.com>
2020-08-20 12:51:00 -0500

    Add files via upload

---
+++ Arreglos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nombres = { "Goku", "Vegetta", "Trunks", "Goten", "Piccolo", "Gohan", "Krillin" };
+            double[] estatura = { 165, 153, 164.5, 164, 180, 156, 176};
+
+            double min = 0;
+            int indice = 0;
+
+
+            //promedio
+            double total = 0;
+            for(int i = 0; i < estatura.Length; i ++)
+            { total += estatura[i]; }
+            double promedio = total / estatura.Length;
+            Console.WriteLine(" el promedio de estaturas es: " + promedio);
+
+            //distancias
+            double[] distancias = new double[estatura.Length];
+            for (int i = 0; i > estatura.Length; i++)
+            {   distancias[i] = Math.Abs(promedio - estatura[i]);
+                if(distancias[i] <min)
+                {
+                    min = distancias[i];
+                    indice = i;
+                }
+                
+            }
+            Console.WriteLine(" el de menor disntacia al promedio es: " + nombres[indice] + " con una estatura de : " + estatura[indice] + " y una distancia de " + min);
+            //organizar
+            for (int j=0; j < estatura.Length; j++)
+            {
+                for (int i = 0; i < estatura.Length -1; i++)
+                { 
+                    if (estatura[i] > estatura[i+1])
+                    {
+                        double tmp = estatura[i];
+                        estatura[i] = estatura[i + 1];
+                        estatura[i + 1] = tmp;
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ Euler y Seno Coseno con ciclo For.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int n = 100;
+            double sum = 0;
+
+            Console.WriteLine(" ingrese valor de x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Double xrad = (180 * x) / Math.PI;
+            //ejercicio 2
+            for (int i = 0; i <= n; i++)
+            {
+                double y = (2 * i);
+                sum += ((Math.Pow(-1, i)) / Factorial(y) )* Math.Pow(xrad, y);
+            }
+            
+            Console.WriteLine(sum);
+
+            //EJERCICIO 1
+            //for (int i = 0; i <= n; i++)
+            //{
+                
+            //    sum += Math.Pow(x, i) / Factorial(i);
+            //}
+            //Console.WriteLine(sum);
+
+        }
+        static double Factorial (double valor)
+        {
+            double total = 1;
+            for (double i = valor; i > 1; i--)
+            {
+                total *= i;
+            }
+            return total;
+        }
+        
+    }
+}
